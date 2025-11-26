acd1244145281c5a25ee0ac4d38d7b8d6426abf3
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-11-26 14:14:12 -0800

    Use AtmosphericMixinConfigPlugin

--- src/main/java/survivalblock/amarong/common/compat/AmarongMixinPlugin.java
+++ src/main/java/survivalblock/amarong/common/compat/AmarongMixinPlugin.java
@@ -1,24 +1,10 @@
 package survivalblock.amarong.common.compat;
 
 import net.fabricmc.loader.api.FabricLoader;
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import survivalblock.amarong.common.Amarong;
+import survivalblock.atmosphere.atmospheric_api.not_mixin.compat.AtmosphericMixinConfigPlugin;
 
-import java.util.List;
-import java.util.Set;
-
-public class AmarongMixinPlugin implements IMixinConfigPlugin {
-
-    @Override
-    public void onLoad(String mixinPackage) {
-    }
-
-    @Override
-    public String getRefMapperConfig() {
-        return null;
-    }
+public class AmarongMixinPlugin implements AtmosphericMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
@@ -32,25 +18,5 @@ public class AmarongMixinPlugin implements IMixinConfigPlugin {
             }
         }
         return true;
-    }
-
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-
-    }
-
-    @Override
-    public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
     }
 }
